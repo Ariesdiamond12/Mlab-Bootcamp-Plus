4d302affc125bfddb8f5b1140f1143d121c088f7
Ariesdiamond12 <92827167+Ariesdiamond12@users.noreply.github.com>
2024-06-09 00:49:12 +0200

    Data types and Operators

---
+++ data-types,operators.js
@@ -0,0 +1,66 @@
+//Exercise 1 - Numbers
+let number1 = 5;
+let number2 = 4.2;
+
+// Addition
+let add = number1 + number2;
+console.log(add);
+// Subtraction
+let sub = number1 - number2;
+console.log(sub);
+// Multiplication
+let multi = number1 * number2;
+console.log(multi);
+// Division
+let div = number1 / number2;
+console.log(div);
+// Modulus
+let mod = number1 % number2;
+console.log(mod);
+// Exponential
+let expo = number1 ** number2;
+console.log(expo);
+
+// Exercise 2 - Boolean and Operators
+
+//Number 1.
+let firstNum = 10;
+let secondNum = 23;
+
+let t = firstNum > secondNum;
+console.log(t);
+let f = secondNum > firstNum;
+console.log(f);
+
+//Number 2.
+let x = 8;
+let y = 12;
+
+console.log(x > y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x != y);
+
+//Number 3.
+
+let a = true;
+let b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+//Number 4.
+
+let p = 10;
+
+p += 6;
+console.log(p);
+p -= 6;
+console.log(p);
+p *= 6;
+console.log(p);
+p /= 6;
+console.log(p);
+p %= 6;
+console.log(p);
