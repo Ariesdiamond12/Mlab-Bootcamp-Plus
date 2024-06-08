29f51de686bc671c590f4197901f100d4fec3009
Ariesdiamond12 <92827167+Ariesdiamond12@users.noreply.github.com>
2024-06-08 20:20:21 +0200

    Variables Exercise

---
+++ variables.js
@@ -0,0 +1,28 @@
+//Exercise 1 - Variable Declaration
+let name = "Tumisho";
+let age = 23;
+let isStudent = true;
+console.log(name, age, isStudent);
+
+//Exercise 2 - Variable Reassignment
+let favouriteColor = "Grey";
+console.log(favouriteColor);
+favouriteColor = "Pink";
+console.log(favouriteColor);
+
+//Exercise 3 - Arithmetic Operations
+let num1 = 12;
+let num2 = 4;
+
+// Sum
+let sum = num1 + num2;
+console.log(sum);
+// Difference
+let diff = num1 - num2;
+console.log(diff);
+// Product
+let pro = num1 * num2;
+console.log(pro);
+// Quotient
+let quo = num1 / num2;
+console.log(quo);
